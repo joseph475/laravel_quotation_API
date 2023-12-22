a5c515cd72ec2162d01aa32bc60623516dc0fc7c
joseph475 <mjc@newswatch.dk>
2023-12-22 22:37:28 +0800

    asd

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -17,7 +17,7 @@ class ItemsController extends Controller
     $data = DB::table('tbl_items')
     ->join('tbl_classifications', 'tbl_items.class_id', '=', 'tbl_classifications.id')
     // ->join('tbl_purchases_receiving', 'tbl_items.id', '=', 'tbl_purchases_receiving.item_id')
-    ->select('tbl_items.*', 'tbl_classifications.*')
+    ->select('tbl_items.*', 'tbl_classifications.name as class_name')
     ->orderBy('tbl_items.itemName', 'asc')
     ->get();
 
